e97c30cd7ab1a724062503e88f1d651065a87494
xieluxin <353018992@qq.com>
2020-01-04 11:04:38 +0800

    图灵机器人集成

--- hdtt-java/src/main/java/com/pagoda/hdtt/HdttConfig.java
+++ hdtt-java/src/main/java/com/pagoda/hdtt/HdttConfig.java
@@ -10,10 +10,12 @@ import com.jfinal.plugin.c3p0.C3p0Plugin;
 import com.jfinal.render.ViewType;
 import com.jfinal.template.Engine;
 import com.pagoda.hdtt.aotogen._MappingKit;
+import com.pagoda.hdtt.controller.DialogController;
 import com.pagoda.hdtt.controller.HelloController;
 import com.pagoda.hdtt.controller.LoginController;
 import com.pagoda.hdtt.interceptor.CrossInterceptor;
 import com.pagoda.hdtt.interceptor.GlobalExceptionInterceptor;
+import com.pagoda.hdtt.invoke.dto.TulingDialogInputDTO;
 
 /**
  * @Author xieluxin
@@ -37,6 +39,7 @@ public class HdttConfig extends JFinalConfig {
     public void configRoute(Routes me) {
         me.add("/template", HelloController.class);
         me.add("/account", LoginController.class);
+        me.add("/dialog", DialogController.class);
     }
 
     @Override
